fd5d42d79169b6cd853dcebeea45199fcc643442
ilan <ilan@spikedata.co.za>
2023-10-09 12:54:45 +0200

    initials

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRequestHeaders } from "#imports";
+const headers = useRequestHeaders(["cookie"]);
+const data = await $fetch("/temp.json", { headers });
+console.log(JSON.stringify(data, null, 2));
+</script>
+
+<template>
+  <div>data</div>
+  <pre
+    >{{ JSON.stringify(data, null, 2) }}
+  </pre>
+</template>
